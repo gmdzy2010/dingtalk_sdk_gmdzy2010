c2a6dbb97e7c2acbd4573dca08caf82871ce2a62
洋哥 <gmdzy2010@users.noreply.github.com>
2018-10-24 10:33:20 +0800

    0.3.0

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
 import setuptools
+
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dingtalk_sdk_gmdzy2010",
+    version="0.3.0",
+    author="gmdzy2010",
+    author_email="gmdzy2010@126.com",
+    description="The non-official sdk of dingtalk",
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='dingtalk',
+    url="https://github.com/gmdzy2010/dingtalk_sdk_gmdzy2010",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
