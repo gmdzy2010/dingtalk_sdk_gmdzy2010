7c7a3dd2e7cdef23f7e440c1272b8d1dae723052
gmdzy2010 <gmdzy2010@126.com>
2018-11-01 10:25:15 +0800

    更改test用例为真实接口用例

--- test_project.py
+++ test_project.py
@@ -1,5 +1,20 @@
-def func(x):
-    return x +1
+from dingtalk_sdk_gmdzy2010.authority_request import AccessTokenRequest
+
+
+appkey = "a key which is definitely invalid"
+appsecret = "repeat those words above."
+agent_id = "test"
+
+
+def set_authorization(appkey, appsecret):
+    params = {"appkey": appkey, "appsecret": appsecret}
+    request = AccessTokenRequest(params=params)
+    request.request_method = "get"
+    response = request.get_json_response()
+    return response
+
 
 def test_answer():
-    assert func(3)==4
+    response = set_authorization(appkey, appsecret)
+    assert response["errmsg"] == "test"
+
